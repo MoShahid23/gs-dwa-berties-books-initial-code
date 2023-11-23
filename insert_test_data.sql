006c3d1fc7ea0848cb0582fd31d948a23ae3f0d0
mo <saadshahidqavi2003@gmail.com>
2023-11-23 02:07:24 +0000

    connecting program to database via new user

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -2,4 +2,10 @@
 
 USE myBookshop;
 
-INSERT INTO books (name, price)VALUES('database book', 40.25),('Node.js book', 25.00), ('Express book', 31.99) ;
+INSERT INTO books (name, price)
+VALUES
+('database book', 40.25),
+('Node.js book', 25.00),
+('Express book', 31.99),
+('Book book', 20.99),
+('Book book 2', 19.99);
